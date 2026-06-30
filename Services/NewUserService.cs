213e8afeb0528e1da5ed2a73f0a1fb2eb6972e2e
Andy Reid <andyrmreid@gmail.com>
2026-06-30 12:58:33 -0400

    Add proper encryption for ADSearcher Password. Style buttons.

--- Services/NewUserService.cs
+++ Services/NewUserService.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.DataProtection;
 using NewUserSite.Data;
 using NewUserSite.Models;
 using System.DirectoryServices;
@@ -12,90 +12,96 @@ namespace NewUserSite.Services
     [SupportedOSPlatform("windows")]
     public class NewUserService
     {
+        private readonly IDataProtectionProvider _provider;
+        public NewUserService(IDataProtectionProvider provider)
+        {
+            _provider = provider;
+        }
         public void CreateNewUser(ADSearcher adSearcher, NewUser newUser)
         {
-
-            using (PrincipalContext principalContext = new PrincipalContext(
-                ContextType.Domain,
-                adSearcher.DomainControllerLocation,
-                adSearcher.DomainControllerRoot,
-                adSearcher.Username,
-                adSearcher.Password
-                ))
-            {
-                Console.WriteLine($"Connected to {adSearcher.DomainControllerLocation}");
-                UserPrincipal templateAccount = UserPrincipal.FindByIdentity(principalContext, newUser.NewUserTemplate.TemplateSAMAccountName) ?? throw new Exception("Template user not found in Active Directory.");
-
-                UserPrincipal? accountCheck = UserPrincipal.FindByIdentity(principalContext, newUser.getSAMAccountName());
-                if (accountCheck != null)
+            var protector = _provider.CreateProtector("ADSearcherPassword");
+                var decryptedPassword = protector.Unprotect(adSearcher.Password);
+                using (PrincipalContext principalContext = new PrincipalContext(
+                    ContextType.Domain,
+                    adSearcher.DomainControllerLocation,
+                    adSearcher.DomainControllerRoot,
+                    adSearcher.Username,
+                    decryptedPassword
+                    ))
                 {
-                    throw new Exception($"An account with the SAM account name {newUser.getSAMAccountName()} already exists in Active Directory.");
-                }
-                else
-                {
-                    Console.WriteLine($"Creating new user {newUser.getSAMAccountName()}");
-                    using (UserPrincipal newUserAccount = new UserPrincipal(principalContext))
+                    Console.WriteLine($"Connected to {adSearcher.DomainControllerLocation}");
+                    UserPrincipal templateAccount = UserPrincipal.FindByIdentity(principalContext, newUser.NewUserTemplate.TemplateSAMAccountName) ?? throw new Exception("Template user not found in Active Directory.");
+
+                    UserPrincipal? accountCheck = UserPrincipal.FindByIdentity(principalContext, newUser.getSAMAccountName());
+                    if (accountCheck != null)
                     {
-                        newUserAccount.Name = newUser.getDisplayName();
-                        newUserAccount.SamAccountName = newUser.getSAMAccountName();
-                        newUserAccount.DisplayName = newUser.getDisplayName();
-                        newUserAccount.GivenName = newUser.FirstName;
-                        newUserAccount.Surname = newUser.LastName;
-                        newUserAccount.Description = templateAccount.Description;
-                        newUserAccount.UserPrincipalName = newUser.getEmailAddress();
-                        newUserAccount.EmailAddress = newUser.getEmailAddress();
-                        newUserAccount.Enabled = true;
-                        newUserAccount.Save();
-
-                        try
-                        {
-                            DirectoryEntry userEntry = newUserAccount.GetUnderlyingObject() as DirectoryEntry;
-                            if (userEntry != null)
-                            {
-                                DirectoryEntry targetOU = new DirectoryEntry($"LDAP://{adSearcher.DomainControllerLocation}/{newUser.ADOrganizationalUnit.ADDistinguishedName}", adSearcher.Username, adSearcher.Password);
-                                userEntry.MoveTo(targetOU);
-                                userEntry.CommitChanges();
-                                targetOU.Close();
-                                Console.WriteLine($"{newUser.getSAMAccountName()} moved to {newUser.ADOrganizationalUnit.Name}");
-                            }
-                        }
-                        catch (COMException comEx)
-                        {
-                            Console.WriteLine($"Failed to move user to target OU: {comEx.Message}");
-                        }
-                        Console.WriteLine($"New user {newUser.getSAMAccountName()} created successfully.");
+                        throw new Exception($"An account with the SAM account name {newUser.getSAMAccountName()} already exists in Active Directory.");
                     }
-                }
-                UserPrincipal targetAccount = UserPrincipal.FindByIdentity(principalContext, newUser.getSAMAccountName());
+                    else
+                    {
+                        Console.WriteLine($"Creating new user {newUser.getSAMAccountName()}");
+                        using (UserPrincipal newUserAccount = new UserPrincipal(principalContext))
+                        {
+                            newUserAccount.Name = newUser.getDisplayName();
+                            newUserAccount.SamAccountName = newUser.getSAMAccountName();
+                            newUserAccount.DisplayName = newUser.getDisplayName();
+                            newUserAccount.GivenName = newUser.FirstName;
+                            newUserAccount.Surname = newUser.LastName;
+                            newUserAccount.Description = templateAccount.Description;
+                            newUserAccount.UserPrincipalName = newUser.getEmailAddress();
+                            newUserAccount.EmailAddress = newUser.getEmailAddress();
+                            newUserAccount.Enabled = true;
+                            newUserAccount.Save();
 
-                var templateUserGroups = templateAccount.GetGroups()
-                    .OfType<GroupPrincipal>()
-                    .Select(g => g.SamAccountName)
-                    .ToList();
+                            try
+                            {
+                                DirectoryEntry userEntry = newUserAccount.GetUnderlyingObject() as DirectoryEntry;
+                                if (userEntry != null)
+                                {
+                                    DirectoryEntry targetOU = new DirectoryEntry($"LDAP://{adSearcher.DomainControllerLocation}/{newUser.ADOrganizationalUnit.ADDistinguishedName}", adSearcher.Username, decryptedPassword);
+                                    userEntry.MoveTo(targetOU);
+                                    userEntry.CommitChanges();
+                                    targetOU.Close();
+                                    Console.WriteLine($"{newUser.getSAMAccountName()} moved to {newUser.ADOrganizationalUnit.Name}");
+                                }
+                            }
+                            catch (COMException comEx)
+                            {
+                                Console.WriteLine($"Failed to move user to target OU: {comEx.Message}");
+                            }
+                            Console.WriteLine($"New user {newUser.getSAMAccountName()} created successfully.");
+                        }
+                    }
+                    UserPrincipal targetAccount = UserPrincipal.FindByIdentity(principalContext, newUser.getSAMAccountName());
 
-                foreach (string groupName in templateUserGroups)
-                {
+                    var templateUserGroups = templateAccount.GetGroups()
+                        .OfType<GroupPrincipal>()
+                        .Select(g => g.SamAccountName)
+                        .ToList();
+
+                    foreach (string groupName in templateUserGroups)
+                    {
                         try
                         {
                             GroupPrincipal group = GroupPrincipal.FindByIdentity(principalContext, IdentityType.SamAccountName, groupName);
                             if (group != null)
-                        {
-                            group.Members.Add(targetAccount);
-                            group.Save();
+                            {
+                                group.Members.Add(targetAccount);
+                                group.Save();
+                            }
                         }
-                    }
-                    catch (PrincipalExistsException)
+                        catch (PrincipalExistsException)
                         {
                             Console.WriteLine($"User {newUser.getSAMAccountName()} is already a member of group {groupName}. Skipping.");
                         }
                         catch (PrincipalOperationException ex)
                         {
                             Console.WriteLine(ex.Message);
-                        Console.WriteLine($"Failed to add user to group {groupName}");
+                            Console.WriteLine($"Failed to add user to group {groupName}");
                         }
                     }
                 }
             }
         }
-
+        
     }
